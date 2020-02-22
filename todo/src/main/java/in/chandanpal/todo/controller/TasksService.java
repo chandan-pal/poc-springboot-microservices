ba37147b902a667ac66dfc7de8ac284a6048ee6c
chandan-pal <chandanpal290190@gmail.com>
2020-02-22 22:17:48 +0530

    bulkhead pattern with hysterix

--- todo/src/main/java/in/chandanpal/todo/controller/TasksService.java
+++ todo/src/main/java/in/chandanpal/todo/controller/TasksService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import in.chandanpal.todo.model.UserTasks;
 
@@ -16,7 +17,14 @@ public class TasksService {
 	public TasksService() {
 	}
 	
-	@HystrixCommand(fallbackMethod = "fallbackTasksInfo")
+	@HystrixCommand(
+			fallbackMethod = "fallbackTasksInfo",
+			threadPoolKey = "userInfoPool",
+			threadPoolProperties = {
+				@HystrixProperty(name = "coreSize", value = "20"),
+				@HystrixProperty(name = "maxQueueSize", value ="10")
+			}
+	)
 	public UserTasks getTasksInfo(String userEmail) {
 		return restTemplate.getForObject("http://tasks-service/user/" + userEmail + "/tasks", UserTasks.class);
 	}
--- todo/src/main/java/in/chandanpal/todo/controller/UserService.java
+++ todo/src/main/java/in/chandanpal/todo/controller/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import in.chandanpal.todo.model.User;
 
@@ -17,7 +18,14 @@ public class UserService {
 	public UserService() {
 	}
 	
-	@HystrixCommand(fallbackMethod = "fallbackUserInfo") 
+	@HystrixCommand(
+			fallbackMethod = "fallbackUserInfo",
+			threadPoolKey = "userInfoPool",
+			threadPoolProperties = {
+				@HystrixProperty(name = "coreSize", value = "20"),
+				@HystrixProperty(name = "maxQueueSize", value ="10")
+			}
+	) 
 	public User getUserInfo(String userEmail) {
 		return restTemplate.getForObject("http://user-service/users/" + userEmail, User.class);
 	}
